45b7d40b4a040dd20972f2b97c8bc3a97f039b39
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2020-12-18 20:25:21 -0300

    importação

--- main.js
+++ main.js
@@ -4,16 +4,8 @@ var btnClean = document.getElementById('clean');
 var input = document.getElementById('txt');
 var lis = ul.querySelectorAll('li');
 
-ul.addEventListener('click',function(e){
-    if(e.target.nodeName === 'LI'){
-        removeEvent(e.target);
-    }
-});
 function addEvent(li){
     li.addEventListener('click',done);
-}
-function removeEvent(li){
-    li.parentNode.removeChild(li);
 }
 btn.addEventListener('click', function(){
     var li = document.createElement('li');
