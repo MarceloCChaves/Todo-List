6e5b745091cf7e937828a375ea096d9762c87d72
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2020-12-22 09:36:49 -0300

    Remove Function addEvent

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ var btnClean = document.getElementById('clean');
 var input = document.getElementById('txt');
 var lis = ul.querySelectorAll('li');
 
-function addEvent(li){
-    li.addEventListener('click',done);
-}
 btn.addEventListener('click', function(){
     var li = document.createElement('li');
     var text = document.createTextNode(input.value);
